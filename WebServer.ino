e65a8d5749e40277e287396baacc052603c79d95
Camila <66568764+CMomesso@users.noreply.github.com>
2023-10-26 22:07:40 -0300

    Add files via upload
    criando um WebServer Assíncrono com Esp32

---
+++ WebServer.ino
@@ -0,0 +1,38 @@
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+// Instanciação do objeto da classe AsyncWebServer
+AsyncWebServer server(80);
+
+const char* ssid = "AirLAN817-933E";
+const char* password = "AirLAN817";
+
+void setup() {
+  // put your setup code here, to run once:
+  // Conecta-se ao Ponto de acesso com as credenciais fornecidas
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(1000);
+  Serial.println("Connecting to WiFi..");
+}
+  
+  // disponibiliza o url "/" com o conteúdo da String abaixo
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(200, "text/plain", "Ola mundo!");
+  });
+  // Servidor começa à ouvir os clientes
+  server.begin();
+
+  // mostra o endereço de IP
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
